c0f8c079b54a9a0c201a8d7178c06857c292bd40
yeyintlwin <yeyintlwin.dev@gmail.com>
2024-06-08 16:41:07 +0900

    2024 Jun 8 : array

---
+++ src/Example131.java
@@ -0,0 +1,15 @@
+
+public class Example131 {
+
+	public static void main(String[] args) {
+		int[] tickets = new int[3];
+		tickets[0] = 1800;
+		tickets[1] = 1200;
+		tickets[2] = 600;
+		System.out.println("大人 : " + tickets[0] + "円");
+		System.out.println("高校生 : " + tickets[1] + "円");
+		System.out.println("中学生 : " + tickets[2] + "円");
+
+	}
+
+}
