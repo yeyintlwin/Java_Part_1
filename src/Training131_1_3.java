e10fd596e2d9fef25616f49230011c52ce0cc2c0
Ye Yint Lwin <yeyintlwin.dev@gmail.com>
2024-06-15 17:30:23 +0900

    Update: Training131_1_3.java
    Added: Comments to realize easily.

--- src/Training131_1_3.java
+++ src/Training131_1_3.java
@@ -8,23 +8,30 @@ public class Training131_1_3 {
 
 		List<Integer> scoresList = new ArrayList<Integer>();
 		Scanner scanner = new Scanner(System.in);
+
+// START : Listen the user inputs and add into array.
 		while (true) {
 			System.out.print("点数　" + (scoresList.size() + 1) + "　入力　>　");
 			int score = scanner.nextInt();
+// When user enter "-1", Calculate the total and average of score.
 			if (score == -1)
 				break;
 			scoresList.add(score);
 		}
+// END : Listen the user inputs and add into array.
 
+// START : Calculate the total of scores.
 		double total = 0;
 		for (int score : scoresList) {
 			total += score;
 		}
-
 		System.out.println("点数会計 : " + total);
+// END : Calculate the total of scores.
 
+// START : Calculate the average of scores.
 		double average = total / scoresList.size();
 		System.out.println("平成点 : " + average);
+// END : Calculate the average of scores.
 
 		scanner.close();
 
