c46578b280c4494519d3469f8c1f113a76afa310
Ye Yint Lwin <yeyintlwin@ubuntu.lan>
2024-06-24 23:06:44 +0900

    Just Testing From Linux

--- src/com/yeyintlwin/Main.java
+++ src/com/yeyintlwin/Main.java
@@ -2,14 +2,18 @@ package com.yeyintlwin;
 
 /**
  * This is the test area of Ye.
- * */
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO: Calculate the different between two dates.
 		String name = "Duake";
-		//Stirng info = STR."My name is \{name}";
-		//System.out.println(info);
+		// Stirng info = STR."My name is \{name}";
+		// System.out.println(info);
+
+		String dataString = "hsvdgfvwuhdfhebwhbfhwb";
+
+		System.out.println("Hello, World!");
 	}
 
 }
