70208db48b3af4baee57dcba281d78bcc028d227
yeyintlwin <yeyintlwin.dev@gmail.com>
2024-09-07 14:20:40 +0900

    modified: Practice0720.java

--- src/Practice0720.java
+++ src/Practice0720.java
@@ -4,6 +4,7 @@ public class Practice0720 {
 	public static final String MY_NAME = "イェ";
 
 	public static void main(String[] args) {
+
 		// 練習1
 		printHello();
 
@@ -93,76 +94,120 @@ public class Practice0720 {
 
 	// 練習5
 	static String getRandomMessage(String name) {
+
+		// create a greeting messages array
 		final String[] messages = { "こんばんは", "こんにちは", "おはよう" };
+
+		// get a random value between from 0 to array size
 		int randomValue = (int) (Math.random() * messages.length);
+
+		// return a random message
 		return messages[randomValue] + name + "さん";
 	}
 
 	// 練習6
 	static double getMinValue(double a, double b) {
+		
+		// return a smallest value
 		return a < b ? a : b;
 	}
 
 	// 練習7
 	static int getMinValue(int[] array) {
+		
+		// get the first index value of array
 		int min = array[0];
+		
+		// get a minimum value from array and set to min
 		for (int n : array)
 			min = n < min ? n : min;
+		
+		// return the minimum value
 		return min;
 	}
 
 	// 問題1
 	static void printRandomMessage() {
+		
+		// create a greeting messages array
 		final String[] messages = { "こんばんは", "こんにちは", "おはよう" };
+		
+		// generate a random value from 0 to size of array
 		int randomValue = (int) (Math.random() * messages.length);
+		
+		// get random message and print message
 		System.out.println(messages[randomValue]);
 	}
 
 	// 問題2
 	static void printRectangleArea(double width, double height) {
+		
+		// calculate the area of a rectangle
 		double areaOfRectangle = width * height;
+		
+		// print the area of rectangle 
 		System.out.println("The area of a rectangle (w:" + width + ", h:" + height + ") is : " + areaOfRectangle);
 	}
 
 	// 問題3
 	static void printMaxValue(double... abc) {
+		
+		// get the first index value of array
 		double max = abc[0];
+		
+		// find a smallest value from array
 		for (double n : abc)
 			max = max > n ? max : n;
+			
+		// print the smallest value
 		System.out.println("The largest value in parameters is : " + max);
 	}
 
 	// 問題4
 	static String getWeatherForecast() {
 
+		// create daysForecast and weatherConditions arrays
 		final String[] daysForecast = { "今日", "明日", "明後日" };
 		final String[] weatherConditions = { "晴れ", "曇り", "雨", "雪" };
 
+		// generate two random values depended on size of receptice array
 		int r1 = (int) (Math.random() * daysForecast.length);
 		int r2 = (int) (Math.random() * weatherConditions.length);
 
+		// create and return an output message
 		return daysForecast[r1] + "の天気は" + weatherConditions[r2] + "でしょう。";
 	}
 
 	// 問題5
 	static void printRandomMessage(String name) {
+		
+		// create greeting messages array
 		final String[] messages = { "こんばんは", "こんにちは", "おはよう" };
+		
+		// generate a random value depend on the size of array
 		int randomValue = (int) (Math.random() * messages.length);
+		
+		// create and print the message
 		System.out.println(messages[randomValue] + name + "さん");
 	}
 
 	// 問題6
 	static double getAbsoluteValue(double value) {
+		
+		// calculate and return the absolute value
 		return value <= 0.0D ? 0.0D - value : value;
 	}
 
 	// 問題7
 	static boolean isEvenNumber(int value) {
+		
+		// decide is the number even or odd 
 		return value % 2 == 0;
 	}
 
 	// 問題8
 	static String getMessage(String name, boolean isKid) {
+		
 		return "こんにちは。" + name + (isKid ? "ちゃん。" : "さん。");
 	}
 
